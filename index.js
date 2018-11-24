b1b0cc9e7b66a399ae45c25abe878a4fa5d23a5f
Eirik Birkeland <EirikBirkeland@users.noreply.github.com>
2018-11-24 03:07:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    function textNodesUnder(el){
-  var n, a = [], walk = document.createTreeWalker(el,NodeFilter.SHOW_TEXT,null,false);
-  while(n = walk.nextNode()) a.push(n);
-  return a;
+document.addEventListener("DOMContentLoaded", function (event) {
+  function textNodesUnder (el) {
+    let n;
+    const a = [];
+    const walk = document.createTreeWalker(el, NodeFilter.SHOW_TEXT, null, false);
+    whil e(n = walk.nextNode()) {
+        a.push(n);
+    }
+    return a;
 }
 
-textNodesUnder(document).map(x=>{
-  if(!x.childNodes.length && x.textContent.match(/Erna/)){
-    x.textContent = "Feit purkehore"
-  }
+  textNodesUnder(document).map(x => {
+    if(!x.childNodes.length && x.textContent.match(/Erna/)){
+      x.textContent = "Feit purkehore"
+    }
+  });
 });
-});
-document.addEventListener(
